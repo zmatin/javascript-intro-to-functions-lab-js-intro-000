8f44ad91c58aa1162573c6c08ea09d749a6ea675
zakir matin <zakir.matin@gmail.com>
2020-01-31 23:38:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,14 +21,3 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   return "I can't hear you!"
   
-}
-
-function sayHiToGrandma(string) {
-  return "I can't hear you!"
-  
-}
-
-function sayHiToGrandma(STRING) {
-  return "I love you, too."
-  
-}
