1a7771c4c64a20119eab06eb011190f0e1cda01d
zakir matin <zakir.matin@gmail.com>
2020-01-31 23:39:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ function logWhisper(string) {
   
 }
 
-function sayHiToGrandma(string) {
-  return "I can't hear you!"
-  
+
+  function sayHiToGrandma(string){
+if (string.toLowerCase() === string){
+return “I can’t hear you!”;
+} if (string.toUpperCase() ===string){
+return “YES INDEED!”;
+} if (string === ‘I love you, Grandma.’){
+return “I love you, too.”;
+}
+}
