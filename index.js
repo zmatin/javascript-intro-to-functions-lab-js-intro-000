a94957713ee5ec973ce9f81e667ed0d0f31a9257
zakir matin <zakir.matin@gmail.com>
2020-01-31 23:16:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase ()
+  
+}
+
+function whisper(string) {
+  return string.toLowerCase ()
+  
+}
+
+function logShout(hello) {
+  console.log(hello.toUpperCase());
+  
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+  
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+  
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!"
+  
+}
