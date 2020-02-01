7d7c43674c30b78ca43dd0163d27de444b73a317
zakir matin <zakir.matin@gmail.com>
2020-02-01 00:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
 } else if (string.toUpperCase){
   return "YES INDEED" 
-} else if (string.)
-  
+} else if (string=== "I love you, Grandma.") {
+  return "I love you, too."
 }
